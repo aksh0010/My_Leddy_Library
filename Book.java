4aa30a907787322a1145060dccca2b154d69b52f
aksh0010 <akshpatelofficial@gmail>
2022-10-22 01:38:40 -0400

    Added getter setter

--- Book.java
+++ Book.java
@@ -18,22 +18,30 @@ public class Book {
   public Book() {}
 
   /**
- * @param name
- * @param author
- * @param isbn
- * @param cost
- * @param publisher
- * @param year
- */
-public Book(String name, String author, int isbn, double cost, String publisher, int year) {
+   * @param name
+   * @param author
+   * @param isbn
+   * @param cost
+   * @param publisher
+   * @param year
+   */
+  public Book(
+    String name,
+    String author,
+    int isbn,
+    double cost,
+    String publisher,
+    int year
+  ) {
     this.name = name;
     this.author = author;
     this.isbn = isbn;
     this.price = cost;
-	this.publisher =publisher;
-	this.year=year;
+    this.publisher = publisher;
+    this.year = year;
   }
 
+  // Setter methods
   public void SetName(String a) {
     this.name = a;
   }
@@ -50,7 +58,39 @@ public Book(String name, String author, int isbn, double cost, String publisher,
     this.price = a;
   }
 
-//   public void print_book() {}
+  public void SetPublisher(String a) {
+    this.publisher = a;
+  }
+
+  public void SetYear(int a) {
+    this.year = a;
+  }
+
+  //   getter methods
+
+  public String getName(String a) {
+    return this.name;
+  }
+
+  public String getAuthor(String a) {
+    return this.author;
+  }
+
+  public int getISBN(int a) {
+    return this.isbn;
+  }
+
+  public double getPrice(double a) {
+    return this.price;
+  }
+
+  public String getPublisher(String a) {
+    return this.publisher;
+  }
+
+  public int getYear(int a) {
+    return this.year;
+  }
 
   /**
    * @param args
